24edd523808e0deace6bb89f60d8ea76e0296f1b
Gvcaw <joe2004young@gmail.com>
2022-07-21 06:40:35 +0000

    .

---
+++ L10-DemoAndroidList/app/src/main/java/com/myapplicationdev/android/demoandroidlist/MainActivity.java
@@ -0,0 +1,68 @@
+package com.myapplicationdev.android.demoandroidlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvAndroidVersions;
+    ArrayList<AndroidVersion> alAndroidVersions;
+    ArrayAdapter<AndroidVersion> aaAndroidVersions;
+
+    //ArrayAdapter<AndroidVersion> aaAndroidVersions;
+
+    CustomAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvAndroidVersions = findViewById(R.id.listViewAndroidVersions);
+        alAndroidVersions = new ArrayList<>();
+
+        //create the objects
+        AndroidVersion item1 = new AndroidVersion("Pie", "9.0");
+        AndroidVersion item2 = new AndroidVersion("Oreo", "8.0 - 8.1");
+        AndroidVersion item3 = new AndroidVersion("Nougat", "7.0 - 7.1.2");
+
+        //adding the objects into the ArrayList
+        alAndroidVersions.add(item1);
+        alAndroidVersions.add(item2);
+        alAndroidVersions.add(item3);
+
+        /*alAndroidVersions.add("Pie - 9.0");
+        alAndroidVersions.add("Oreo - 8.0 - 8.1");
+        alAndroidVersions.add("Nougat - 7.0 - 7.1.2");
+        alAndroidVersions.add("Marshmallow - 6.0 - 6.0.1");
+        alAndroidVersions.add("Lollipop - 5.0 - 5.1.1");
+        alAndroidVersions.add("KitKat - 4.4 - 4.4.4");
+        alAndroidVersions.add("Jelly Bean - 4.1 - 4.3.1");
+        */
+        aaAndroidVersions = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, alAndroidVersions);
+
+        lvAndroidVersions.setAdapter(aaAndroidVersions);
+
+
+        //aaAndroidVersions = new ArrayAdapter<>(this,
+        //android.R.layout.simple_list_item_1, alAndroidVersions);
+
+
+        adapter = new CustomAdapter(this, R.layout.row, alAndroidVersions);
+
+
+        //lvAndroidVersions.setAdapter(aaAndroidVersions);
+
+
+        lvAndroidVersions.setAdapter(adapter);
+
+    }
+
+}
---
+++ P10/P10- DemoToDoList/app/src/main/java/sg/edu/rp/c346/id21024429/demotodolisr/MainActivity.java
@@ -0,0 +1,46 @@
+package sg.edu.rp.c346.id21024429.demotodolisr;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvToDo;
+    ArrayList<ToDoItem> alToDoList;
+    ArrayAdapter<ToDoItem> aaToDoList;
+    CustomAdapter caToDo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvToDo = findViewById(R.id.listViewToDo);
+
+        alToDoList = new ArrayList<>();
+        Calendar date1 = Calendar.getInstance();
+        date1.set(2020,8,1);
+        Calendar date2 = Calendar.getInstance();
+        date2.set(2020,8,2);
+        ToDoItem item1 = new ToDoItem("Go for movie", date1);
+        ToDoItem item2 = new ToDoItem("Go for haircut", date2);
+        alToDoList.add(item1);
+        alToDoList.add(item2);
+
+        aaToDoList = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, alToDoList);
+
+        lvToDo.setAdapter(aaToDoList);
+
+        caToDo = new CustomAdapter(this, R.layout.row, alToDoList);
+
+        lvToDo.setAdapter(caToDo);
+
+    }
+}
